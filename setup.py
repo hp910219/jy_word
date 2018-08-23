71f3f7f804ddb32d48774341db0cd0d5d4bd783f
hp910219 <792089975@qq.com>
2018-08-23 17:13:08 +0800

    jy_word0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jy-word',
-    version='0.6',
+    version='0.7',
     keywords=('word', 'test'),
     description='generate word',
     license='MIT License',
