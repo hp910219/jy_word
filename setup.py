983fc2a2e924e92c0c3f2972e5ed863577081b76
hp910219 <792089975@qq.com>
2018-08-23 14:52:42 +0800

    jy_word

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ __author__ = 'huohuo'
 from setuptools import setup, find_packages
 
 setup(
-    name = 'jy_word',
-    version = '0.0.2',
+    name = 'jy-word',
+    version = '0.2',
     keywords = ('word', 'test'),
     description = 'generate word',
     license = 'MIT License',
 
     author = 'hp910219',
     author_email = 'hp910219@126.com',
-    url='https://github.com/hp910219/jy_word.git',
+    url='https://github.com/hp910219/jy-word.git',
 
     packages = find_packages(exclude=['test']),
 )
